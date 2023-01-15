f917241b6848eb26cb38e2525028025aeab0455b
emifalcon <109958452+emifalcon@users.noreply.github.com>
2023-01-15 16:34:03 -0300

    Ejercicio8

---
+++ N32cicloadivinarN.java
@@ -0,0 +1,20 @@
+/*ejercicio 5 : realizar un juego para adivinar un numero.Para ello generar un numero
+aleatorio entre 0-100 y luego pedir numeros indicando si es"mayor"o"menor" con respecto
+a N.El proceso termina cuando el usuario acierta y mostrar el numero de intentos.
+ */
+package com.mycompany.n32cicloadivinarn;
+
+/**
+ *
+ * @author usuario
+ */
+public class N32cicloadivinarN {
+
+    public static void main(String[] args) {
+
+        int numero, contador;
+        double aleatorio;
+
+        System.out.println("Hello World!");
+    }
+}
