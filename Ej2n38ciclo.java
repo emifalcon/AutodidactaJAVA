a840387baee94c60513687e2c064e5f91587e070
emifalcon <109958452+emifalcon@users.noreply.github.com>
2023-01-15 17:16:28 -0300

    Ejercicio14

---
+++ Ej2n38ciclo.java
@@ -0,0 +1,19 @@
+/*
+ejrcicio: Hace modificaciones al ejercicio anterior , haciendo que no se coloque 
+el precio por litro. Se dispone de solo 3 opciones:
+1- 0,6$/litro , 2- 3$litro , 3- 1,25euro/litro.
+*/
+ 
+
+package com.mycompany.ej2n38ciclo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ej2n38ciclo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
