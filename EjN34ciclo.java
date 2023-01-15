58a36ecd51d8aa4d38c1a8047c57fe406e894dbd
emifalcon <109958452+emifalcon@users.noreply.github.com>
2023-01-15 17:13:19 -0300

    Ejercicio12

---
+++ EjN34ciclo.java
@@ -0,0 +1,29 @@
+/*ejercicio: escribir todos los numeros del 100 al 0 en 7 en 7.
+
+*/
+ 
+
+package com.mycompany.ej.n34ciclo;
+
+/**
+ *
+ * @author usuario
+ */
+public class EjN34ciclo {
+
+    public static void main(String[] args) {
+        
+       
+        
+        
+        for( int num = 100; num>=0;num-=7)
+        
+        
+        
+        
+        
+        
+        
+        System.out.println(num);
+    }
+}
